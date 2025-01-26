b6526f27ad9f715f170fd8b20425146edcbf650b
Azure pipeline <you@example.com>
2025-01-26 01:34:38 +0000

    Automatically updating web files for datashield

--- tags/Study: KANC/stats.js
+++ tags/Study: KANC/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2021',1],['2022',1],['2023',0],['2024',2],]);var citations =([['Year', 'Number of Citations'],['2021',33],['2022',21],['2023',0],['2024',2],]);
+var papers =([['Year', 'Number of papers'],['2021',1],['2022',1],['2023',0],['2024',2],]);var citations =([['Year', 'Number of Citations'],['2021',34],['2022',22],['2023',0],['2024',3],]);
