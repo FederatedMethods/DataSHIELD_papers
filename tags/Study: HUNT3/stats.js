7da39f79cffb95d487832f291a89d1aebd720c84
Azure pipeline <you@example.com>
2025-05-18 01:34:15 +0000

    Automatically updating web files for datashield

--- tags/Study: HUNT3/stats.js
+++ tags/Study: HUNT3/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2016',27],['2017',243],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2016',28],['2017',243],]);
