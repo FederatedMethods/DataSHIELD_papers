7ad48665b9ffefbf97082c435147394cb93f0eb5
Azure pipeline <you@example.com>
2025-06-01 01:34:39 +0000

    Automatically updating web files for datashield

--- keywords/Noise, Transportation/stats.js
+++ keywords/Noise, Transportation/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',1],]);var citations =([['Year', 'Number of Citations'],['2016',28],['2017',139],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',1],]);var citations =([['Year', 'Number of Citations'],['2016',0],['2017',0],]);
