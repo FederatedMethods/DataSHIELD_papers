467991ae3e50d911815db5e6cabbc0755359b77e
Azure pipeline <you@example.com>
2025-09-23 11:23:01 +0000

    Automatically updating web files for datashield

--- keywords/COVID-19/stats.js
+++ keywords/COVID-19/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2021',2],]);var citations =([['Year', 'Number of Citations'],['2021',32],]);
+var papers =([['Year', 'Number of papers'],['2021',2],['2022',0],['2023',0],['2024',0],['2025',2],]);var citations =([['Year', 'Number of Citations'],['2021',32],['2022',0],['2023',0],['2024',0],['2025',0],]);
