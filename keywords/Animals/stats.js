c080b3ac32f95e9371c4a3bcdde7c943a1785a82
Azure pipeline <you@example.com>
2025-10-12 01:34:46 +0000

    Automatically updating web files for datashield

--- keywords/Animals/stats.js
+++ keywords/Animals/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2021',1],['2022',1],['2023',1],]);var citations =([['Year', 'Number of Citations'],['2021',11],['2022',37],['2023',31],]);
+var papers =([['Year', 'Number of papers'],['2021',1],['2022',1],['2023',1],]);var citations =([['Year', 'Number of Citations'],['2021',11],['2022',37],['2023',32],]);
--- tags/Study: HUNT3/stats.js
+++ tags/Study: HUNT3/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2016',1],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2016',32],['2017',247],]);
+var papers =([['Year', 'Number of papers'],['2016',1],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2016',32],['2017',248],]);
