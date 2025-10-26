a74e059d51036b8f047617907e1f9de9dc6924df
Azure pipeline <you@example.com>
2025-10-26 01:34:27 +0000

    Automatically updating web files for datashield

--- tags/Consortia: Welsh and Scottish Farr Institutes/stats.js
+++ tags/Consortia: Welsh and Scottish Farr Institutes/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',1],['2015',2],['2016',3],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2014',210],['2015',87],['2016',54],['2017',259],]);
+var papers =([['Year', 'Number of papers'],['2014',1],['2015',2],['2016',3],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2014',211],['2015',87],['2016',54],['2017',259],]);
