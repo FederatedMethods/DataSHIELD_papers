6281aa2cf5ac1ae2245364970abb900e3e9ef60c
Azure pipeline <you@example.com>
2025-11-05 11:06:41 +0000

    Automatically updating web files for datashield

--- keywords/Body Mass Index/stats.js
+++ keywords/Body Mass Index/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2023',2],['2024',1],]);var citations =([['Year', 'Number of Citations'],['2023',26],['2024',4],]);
+var papers =([['Year', 'Number of papers'],['2023',2],['2024',1],['2025',1],]);var citations =([['Year', 'Number of Citations'],['2023',26],['2024',4],['2025',0],]);
