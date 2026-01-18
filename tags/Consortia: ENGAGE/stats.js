38c604dc74e8ac5249a2491f450f866426010c70
Azure pipeline <you@example.com>
2026-01-18 01:42:47 +0000

    Automatically updating web files for datashield

--- tags/Consortia: ENGAGE/stats.js
+++ tags/Consortia: ENGAGE/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2012',2],['2013',0],['2014',0],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2012',145],['2013',0],['2014',0],['2015',32],]);
+var papers =([['Year', 'Number of papers'],['2012',2],['2013',0],['2014',0],['2015',1],]);var citations =([['Year', 'Number of Citations'],['2012',146],['2013',0],['2014',0],['2015',32],]);
