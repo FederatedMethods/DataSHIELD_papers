640afb7d8e354b3344be22b4077f7230accb3295
Azure pipeline <you@example.com>
2026-01-25 01:34:40 +0000

    Automatically updating web files for datashield

--- keywords/United Kingdom/stats.js
+++ keywords/United Kingdom/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',2],]);var citations =([['Year', 'Number of Citations'],['2014',244],]);
+var papers =([['Year', 'Number of papers'],['2014',2],]);var citations =([['Year', 'Number of Citations'],['2014',245],]);
--- tags/Consortia: BBMRI-LPC/stats.js
+++ tags/Consortia: BBMRI-LPC/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',1],['2015',1],['2016',2],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2014',219],['2015',56],['2016',22],['2017',266],]);
+var papers =([['Year', 'Number of papers'],['2014',1],['2015',1],['2016',2],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2014',220],['2015',56],['2016',22],['2017',268],]);
