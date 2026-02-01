95de4488dbf45baf5ca49f1c439589af21f2c0df
Azure pipeline <you@example.com>
2026-02-01 01:42:10 +0000

    Automatically updating web files for datashield

--- tags/Consortia: BBMRI-LPC/stats.js
+++ tags/Consortia: BBMRI-LPC/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2014',1],['2015',1],['2016',2],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2014',220],['2015',56],['2016',22],['2017',268],]);
+var papers =([['Year', 'Number of papers'],['2014',1],['2015',1],['2016',2],['2017',3],]);var citations =([['Year', 'Number of Citations'],['2014',221],['2015',56],['2016',22],['2017',269],]);
