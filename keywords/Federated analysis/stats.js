e2c509c041407b127243599d753103287542ced7
Azure pipeline <you@example.com>
2026-02-05 10:47:21 +0000

    Automatically updating web files for datashield

--- keywords/Federated analysis/stats.js
+++ keywords/Federated analysis/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2022',2],['2023',0],['2024',0],['2025',2],]);var citations =([['Year', 'Number of Citations'],['2022',18],['2023',0],['2024',0],['2025',0],]);
+var papers =([['Year', 'Number of papers'],['2022',2],['2023',0],['2024',0],['2025',3],]);var citations =([['Year', 'Number of Citations'],['2022',18],['2023',0],['2024',0],['2025',0],]);
