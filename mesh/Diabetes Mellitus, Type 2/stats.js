de84551d3a1022080bc3d38d029da071b2fa3de1
Azure pipeline <you@example.com>
2026-03-15 01:34:54 +0000

    Automatically updating web files for datashield

--- mesh/Diabetes Mellitus, Type 2/stats.js
+++ mesh/Diabetes Mellitus, Type 2/stats.js
@@ -1,1 +1,1 @@
-var papers =([['Year', 'Number of papers'],['2021',2],['2022',1],['2023',1],['2024',2],]);var citations =([['Year', 'Number of Citations'],['2021',105],['2022',9],['2023',33],['2024',16],]);
+var papers =([['Year', 'Number of papers'],['2021',2],['2022',1],['2023',1],['2024',2],]);var citations =([['Year', 'Number of Citations'],['2021',105],['2022',9],['2023',34],['2024',17],]);
